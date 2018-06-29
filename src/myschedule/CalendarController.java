e2fbfa8f7158bc23c8a5599ad2eedebc136abc32
PREDATOR-LAPTOP\bradd <brad.duderstadt@gmail.com>
2018-06-29 07:24:40 -0500

    Begin implementation of inner classes for calendar views

--- src/myschedule/CalendarController.java
+++ src/myschedule/CalendarController.java
@@ -24,11 +24,24 @@
 package myschedule;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.logging.Level;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import myschedule.model.AppointmentModel;
 
 /**
  * @author bradd
@@ -79,16 +92,167 @@ public class CalendarController {
         
         if (version.equals("month")) {
             MonthView monthView = new MonthView(YearMonth.now());
-            monthView.injectApp(app);
-            monthView.injectMainController(main);
             monthView.buildCalendar();
             calendarPane.getChildren().add(monthView.getView());
         }
         else {
             MonthView monthView = new MonthView(YearMonth.now());
-            monthView.injectApp(app);
-            monthView.injectMainController(main);
             calendarPane.getChildren().add(monthView.getView());
+        }
+    }
+    
+    // Define MonthView as inner class of CalendarController class
+    public class MonthView {
+        private ArrayList<AnchorPaneNode> allCalendarDays = new ArrayList<>(35);
+        private VBox view;
+        private Text calendarTitle;
+        private YearMonth currentYearMonth;
+
+        private ObservableList<AppointmentModel> appointmentList = FXCollections.observableArrayList();
+
+        /**
+         * Create a calendar view
+         * @param yearMonth year month to create the calendar of
+         */
+        public MonthView(YearMonth yearMonth) {
+            currentYearMonth = yearMonth;
+        }
+
+        public void buildCalendar() {
+            // Create the calendar grid pane
+            GridPane calendar = new GridPane();
+            calendar.setPrefSize(500, 200);
+            calendar.setGridLinesVisible(true);
+
+            // Create rows and columns with each cell being an anchor pane
+            // for a given calendar date
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 7; j++) {
+                    AnchorPaneNode ap = new AnchorPaneNode();
+                    ap.setPrefSize(200,200);
+                    calendar.add(ap, j, i);
+                    allCalendarDays.add(ap);
+                }
+            }
+
+            // Days of the week labels
+            Text[] dayNames = new Text[]{ new Text("Sun"), new Text("Mon"), new Text("Tue"),
+                new Text("Wed"), new Text("Thu"), new Text("Fri"), new Text("Sat") };
+            GridPane dayLabels = new GridPane();
+            dayLabels.setPrefWidth(500);
+            Integer col = 0;
+
+            // Add DOW column headers
+            for (Text txt : dayNames) {
+                AnchorPane ap = new AnchorPane();
+                ap.setPrefSize(200, 10);
+                AnchorPane.setBottomAnchor(txt, 5.0);
+                ap.getChildren().add(txt);
+                dayLabels.add(ap, col++, 0);
+            }
+
+            // Create calendarTitle and buttons to change current month
+            calendarTitle = new Text();
+            Button previousMonth = new Button("<<");
+            previousMonth.setOnAction(e -> previousMonth());
+            Button nextMonth = new Button(">>");
+            nextMonth.setOnAction(e -> nextMonth());
+            HBox titleBar = new HBox();
+            titleBar.setSpacing(10);
+            titleBar.getChildren().addAll(previousMonth, calendarTitle, nextMonth);
+            titleBar.setAlignment(Pos.BASELINE_CENTER);
+
+            // Populate calendar with the appropriate day numbers
+            populateCalendar(currentYearMonth);
+
+            // Create HBox for spacing between titleBar and dayLabels
+            Region spacer = new Region();
+            spacer.setPrefSize(100.0, 20.0);
+
+            // Create the calendar view
+            view = new VBox(titleBar, spacer, dayLabels, calendar);
+        }
+
+        /**
+         * Set the days of the calendar to correspond to the appropriate date
+         * @param yearMonth year and month of month to render
+         */
+        public void populateCalendar(YearMonth yearMonth) {
+            String mm;
+            String yyyy;
+    //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    //        String dd = Integer.toString(LocalDate.now().getDayOfMonth());
+    //        String mm = Integer.toString(LocalDate.now().getMonthValue());
+    //        ResultSet rs;
+    //        String today = LocalDate.now().format(formatter);
+    //        String yyyy = Integer.toString(LocalDate.now().getYear());
+
+            // Get the date we want to start with on the calendar. calendarDate ends up being the first
+            // of the month for the current month (or chosen month) hence the "1" second parameter below 
+            LocalDate calendarDate = LocalDate.of(yearMonth.getYear(), yearMonth.getMonthValue(), 1);
+            yyyy = Integer.toString(yearMonth.getYear());
+            mm = yearMonth.getMonthValue() < 10 ? "0" + Integer.toString(yearMonth.getMonthValue()) : Integer.toString(yearMonth.getMonthValue());
+
+            // Get resultset of appointments for given month / year
+            try {
+                appointmentList = app.db.getAppointments(mm, yyyy);
+            }
+            catch (SQLException ex) {
+                app.common.alertStatus(0);
+            }
+
+            // Dial back the day until it is SUNDAY (unless the month starts on a sunday)
+            while (!calendarDate.getDayOfWeek().toString().equals("SUNDAY") ) {
+                calendarDate = calendarDate.minusDays(1);
+            }
+
+            // Populate the calendar with day numbers
+            for (AnchorPaneNode ap : allCalendarDays) {
+                if (!ap.getChildren().isEmpty()) {
+                    ap.getChildren().remove(0);
+                }
+
+               // This is where to lookup month & day to see if there's an appointment by using
+               // calendar.getMonthValue() - int
+               // calendar.getDayOfMonth() - int
+
+                Text txt = new Text(String.valueOf(calendarDate.getDayOfMonth()));
+                ap.setDate(calendarDate);
+                AnchorPaneNode.setTopAnchor(txt, 5.0);
+                AnchorPaneNode.setLeftAnchor(txt, 5.0);
+                ap.getChildren().add(txt);
+                calendarDate = calendarDate.plusDays(1);
+            }
+            // Change the title of the calendar
+            calendarTitle.setText(yearMonth.getMonth().toString() + " " + String.valueOf(yearMonth.getYear()));
+        }
+
+        /**
+         * Move the month back by one. Re-populate the calendar with the correct dates.
+         */
+        private void previousMonth() {
+            currentYearMonth = currentYearMonth.minusMonths(1);
+            populateCalendar(currentYearMonth);
+        }
+
+        /**
+         * Move the month forward by one. Re-populate the calendar with the correct dates.
+         */
+        private void nextMonth() {
+            currentYearMonth = currentYearMonth.plusMonths(1);
+            populateCalendar(currentYearMonth);
+        }
+
+        public VBox getView() {
+            return view;
+        }
+
+        public ArrayList<AnchorPaneNode> getAllCalendarDays() {
+            return allCalendarDays;
+        }
+
+        public void setAllCalendarDays(ArrayList<AnchorPaneNode> allCalendarDays) {
+            this.allCalendarDays = allCalendarDays;
         }
     }
 }
