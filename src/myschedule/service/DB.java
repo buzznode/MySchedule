10dd863884741f6e190054c73c07da62c4266460
PREDATOR-LAPTOP\bradd <brad.duderstadt@gmail.com>
2018-07-09 06:52:06 -0500

    Removed credentials from DB

--- src/myschedule/service/DB.java
+++ src/myschedule/service/DB.java
@@ -65,11 +65,11 @@ public class DB {
      */
     public DB() {
         conn = null;
-        db = "U03MuY";
-        dbPwd = "53688020218";
-        dbUser = "U03MuY";
-        driver = "com.mysql.jdbc.Driver";
-        url = "jdbc:mysql://52.206.157.109/" + db;
+        db = null;
+        dbPwd = null;
+        dbUser = null;
+        driver = null;
+        url = null + db;
     }
     
     /**
@@ -78,12 +78,12 @@ public class DB {
      */
     public DB(Logging _log) {
         conn = null;
-        db = "U03MuY";
-        dbPwd = "53688020218";
-        dbUser = "U03MuY";
-        driver = "com.mysql.jdbc.Driver";
+        db = null;
+        dbPwd = null;
+        dbUser = null;
+        driver = null;
         log = _log;
-        url = "jdbc:mysql://52.206.157.109/" + db;
+        url = null + db;
     }
     
     /**
